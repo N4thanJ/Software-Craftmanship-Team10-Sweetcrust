790edb0b8d4459c7159fc8b032e6dfff1a336283
xanderdrvr <xander.deroover@student.ucll.be>
2025-10-16 13:31:32 +0200

    Update ids to id records

--- src/main/java/be/ucll/team10_bakery/product/domain/entities/Product.java
+++ src/main/java/be/ucll/team10_bakery/product/domain/entities/Product.java
@@ -2,19 +2,16 @@ package be.ucll.team10_bakery.product.domain.entities;
 
 import java.util.UUID;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import be.ucll.team10_bakery.product.domain.valueobjects.ProductId;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "products")
 public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+
+    @EmbeddedId
+    private ProductId id;
 
     @NotBlank(message = "Name of product should not be empty")
     private String name;
@@ -23,12 +20,8 @@ public class Product {
         setName(name);
     }
 
-    public UUID getId() {
+    public ProductId getId() {
         return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
     }
 
     public String getName() {
--- src/main/java/be/ucll/team10_bakery/user/domain/entities/User.java
+++ src/main/java/be/ucll/team10_bakery/user/domain/entities/User.java
@@ -2,19 +2,16 @@ package be.ucll.team10_bakery.user.domain.entities;
 
 import java.util.UUID;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import be.ucll.team10_bakery.user.domain.valueobjects.UserId;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "users")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+
+    @EmbeddedId
+    private UserId id;
 
     @NotBlank(message = "Username should not be blank")
     private String username;
@@ -27,12 +24,8 @@ public class User {
         setPassword(password);
     }
 
-    public UUID getId() {
+    public UserId getId() {
         return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
     }
 
     public String getUsername() {
