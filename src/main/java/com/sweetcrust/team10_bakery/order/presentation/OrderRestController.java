dc9b66f31cc353ba9127a16ba2505f5da69942a7
Rajo <rajo.timmermans@gmail.com>
2025-10-21 11:20:34 +0200

    commit before branch pull

--- src/main/java/com/sweetcrust/team10_bakery/order/presentation/OrderRestController.java
+++ src/main/java/com/sweetcrust/team10_bakery/order/presentation/OrderRestController.java
@@ -1,4 +1,45 @@
 package com.sweetcrust.team10_bakery.order.presentation;
 
+import com.sweetcrust.team10_bakery.order.application.OrderCommandService;
+import com.sweetcrust.team10_bakery.order.application.OrderQueryService;
+import com.sweetcrust.team10_bakery.order.domain.entities.Order;
+import com.sweetcrust.team10_bakery.order.domain.valueobjects.OrderId;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/order")
+@Tag(name = "Order Management", description = "Endpoints related to order status fetching en orderItems")
 public class OrderRestController {
+
+    private final OrderCommandService orderCommandService;
+    private final OrderQueryService orderQueryService;
+
+    @Autowired
+    public OrderRestController(OrderCommandService orderCommandService, OrderQueryService orderQueryService) {
+        this.orderCommandService = orderCommandService;
+        this.orderQueryService = orderQueryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Order>> getAllOrders() {
+        List<Order> orders = orderQueryService.getAllOrders();
+        return ResponseEntity.ok(orders);
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> getOrderById(@PathVariable UUID orderId) {
+        Order order = orderQueryService.getOrderById(new OrderId(orderId));
+        return ResponseEntity.ok(order);
+    }
+
+
 }
--- src/main/java/com/sweetcrust/team10_bakery/shared/infrastructure/DbInitializer.java
+++ src/main/java/com/sweetcrust/team10_bakery/shared/infrastructure/DbInitializer.java
@@ -1,7 +1,17 @@
 package com.sweetcrust.team10_bakery.shared.infrastructure;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
+import com.sweetcrust.team10_bakery.order.domain.entities.Order;
+import com.sweetcrust.team10_bakery.order.domain.valueobjects.DeliveryAddress;
+import com.sweetcrust.team10_bakery.order.domain.valueobjects.OrderType;
+import com.sweetcrust.team10_bakery.order.infrastructure.OrderRepository;
+import com.sweetcrust.team10_bakery.shop.domain.entities.Shop;
+import com.sweetcrust.team10_bakery.user.domain.entities.User;
+import com.sweetcrust.team10_bakery.user.domain.valueobjects.UserId;
+import com.sweetcrust.team10_bakery.user.domain.valueobjects.UserRole;
+import com.sweetcrust.team10_bakery.user.infrastructure.UserRepository;
 import org.springframework.stereotype.Component;
 
 import com.sweetcrust.team10_bakery.product.domain.entities.Product;
@@ -13,9 +23,13 @@ import jakarta.annotation.PostConstruct;
 @Component
 public class DbInitializer {
     private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
 
-    public DbInitializer(ProductRepository productRepository) {
+    public DbInitializer(ProductRepository productRepository, OrderRepository orderRepository, UserRepository userRepository) {
         this.productRepository = productRepository;
+        this.orderRepository = orderRepository;
+        this.userRepository = userRepository;
     }
 
     private void clearAll() {
@@ -26,6 +40,7 @@ public class DbInitializer {
     public void init() {
         clearAll();
 
+
         ProductCategory productCategory = new ProductCategory("Cakes", "A variety of cakes");
 
         Product curryCake = new Product("Curry Cake Delux", "A very delicious originated from India dish",
@@ -33,5 +48,14 @@ public class DbInitializer {
                 productCategory.getCategoryId());
 
         productRepository.save(curryCake);
+
+        User user1 = new User("Curry Lover", "IloveCurry123!", "curry.lover@sweetcrust.in", UserRole.CUSTOMER);
+
+        userRepository.save(user1);
+
+        Order b2cOrder = Order.createB2C(OrderType.B2C, new DeliveryAddress("CurryStreet", "CurryCity", "12345", "CurryCountry"), LocalDateTime.now().plusDays(2), user1.getUserId());
+        Order b2bOrder = Order.createB2B(OrderType.B2B, LocalDateTime.now().plusDays(5), null, null);
+
+
     }
 }
