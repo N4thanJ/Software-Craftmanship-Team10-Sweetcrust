c0facb6d3dddac285303deb1f509c1dcc48c8f4c
N4thanJ <nathan.jordens@student.ucll.be>
2025-10-21 10:55:19 +0200

    Added a product in DbInit

---
+++ src/main/java/com/sweetcrust/team10_bakery/shared/infrastructure/DbInitializer.java
@@ -0,0 +1,37 @@
+package com.sweetcrust.team10_bakery.shared.infrastructure;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Component;
+
+import com.sweetcrust.team10_bakery.product.domain.entities.Product;
+import com.sweetcrust.team10_bakery.product.domain.entities.ProductCategory;
+import com.sweetcrust.team10_bakery.product.infrastructure.ProductRepository;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class DbInitializer {
+    private final ProductRepository productRepository;
+
+    public DbInitializer(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    private void clearAll() {
+        productRepository.deleteAll();
+    }
+
+    @PostConstruct
+    public void init() {
+        clearAll();
+
+        ProductCategory productCategory = new ProductCategory("Cakes", "A variety of cakes");
+
+        Product curryCake = new Product("Curry Cake Delux", "A very delicious originated from India dish",
+                BigDecimal.valueOf(4.99), true,
+                productCategory.getCategoryId());
+
+        productRepository.save(curryCake);
+    }
+}
