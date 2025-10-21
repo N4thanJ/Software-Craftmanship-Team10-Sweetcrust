539186f12a7df77d1598979ec3c032a12b58be57
Sander Coemans <sander.coemans@student.ucll.be>
2025-10-21 11:45:25 +0200

    addshopcommand

--- src/main/java/com/sweetcrust/team10_bakery/shop/application/commands/AddShopCommand.java
+++ src/main/java/com/sweetcrust/team10_bakery/shop/application/commands/AddShopCommand.java
@@ -1,8 +1,12 @@
 package com.sweetcrust.team10_bakery.shop.application.commands;
 
+import com.sweetcrust.team10_bakery.shop.domain.valueobjects.CountryCode;
+import com.sweetcrust.team10_bakery.shop.domain.valueobjects.ShopAddress;
+
 public record AddShopCommand(
         String name,
+        ShopAddress address,
         String email,
-
+        CountryCode countryCode
 ) {
 }
