2d0243ec39a1fc4eb2b16e6acbac8bd24d1f2523
Sander Coemans <sander.coemans@student.ucll.be>
2025-10-21 11:16:06 +0200

    get shops

--- src/main/java/com/sweetcrust/team10_bakery/shared/infrastructure/DbInitializer.java
+++ src/main/java/com/sweetcrust/team10_bakery/shared/infrastructure/DbInitializer.java
@@ -2,6 +2,9 @@ package com.sweetcrust.team10_bakery.shared.infrastructure;
 
 import java.math.BigDecimal;
 
+import com.sweetcrust.team10_bakery.shop.domain.entities.Shop;
+import com.sweetcrust.team10_bakery.shop.domain.valueobjects.ShopAddress;
+import com.sweetcrust.team10_bakery.shop.infrastructure.ShopRepository;
 import org.springframework.stereotype.Component;
 
 import com.sweetcrust.team10_bakery.product.domain.entities.Product;
@@ -13,9 +16,11 @@ import jakarta.annotation.PostConstruct;
 @Component
 public class DbInitializer {
     private final ProductRepository productRepository;
+    private final ShopRepository shopRepository;
 
-    public DbInitializer(ProductRepository productRepository) {
+    public DbInitializer(ProductRepository productRepository,  ShopRepository shopRepository) {
         this.productRepository = productRepository;
+        this.shopRepository = shopRepository;
     }
 
     private void clearAll() {
@@ -33,5 +38,8 @@ public class DbInitializer {
                 productCategory.getCategoryId());
 
         productRepository.save(curryCake);
+
+        Shop indiaShop = new Shop("SweetCrust New Delhi", new ShopAddress("Currystreet 1", "New Delhi", "110001", "India"), "newdelhi@sweetcrust.com");
+        shopRepository.save(indiaShop);
     }
 }
--- src/main/java/com/sweetcrust/team10_bakery/shop/presentation/ShopRestController.java
+++ src/main/java/com/sweetcrust/team10_bakery/shop/presentation/ShopRestController.java
@@ -1,4 +1,41 @@
 package com.sweetcrust.team10_bakery.shop.presentation;
 
+import com.sweetcrust.team10_bakery.shop.application.ShopCommandService;
+import com.sweetcrust.team10_bakery.shop.application.ShopQueryService;
+import com.sweetcrust.team10_bakery.shop.domain.entities.Shop;
+import com.sweetcrust.team10_bakery.shop.domain.valueobjects.ShopId;
+import com.sweetcrust.team10_bakery.user.domain.entities.User;
+import com.sweetcrust.team10_bakery.user.domain.valueobjects.UserId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/shop")
 public class ShopRestController {
+    private final ShopCommandService shopCommandService;
+    private final ShopQueryService shopQueryService;
+
+    public ShopRestController(ShopCommandService shopCommandService, ShopQueryService shopQueryService) {
+        this.shopCommandService = shopCommandService;
+        this.shopQueryService = shopQueryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Shop>> findAll() {
+        List<Shop> shops = shopQueryService.getAllShops();
+        return ResponseEntity.ok(shops);
+    }
+
+    @GetMapping("/{shopId}")
+    public ResponseEntity<Shop> getShopById(@PathVariable UUID shopId) {
+        Shop shop = shopQueryService.getShopById(new ShopId(shopId));
+        return ResponseEntity.ok(shop);
+    }
 }
