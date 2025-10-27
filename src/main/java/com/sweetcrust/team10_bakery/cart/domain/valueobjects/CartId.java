c1396c1560a4d76623b921d4d18d9193cd161bc3
xanderdrvr <xander.deroover@student.ucll.be>
2025-10-27 15:39:29 +0100

    Add file structure for cart implementation

---
+++ src/main/java/com/sweetcrust/team10_bakery/cart/domain/valueobjects/CartId.java
@@ -0,0 +1,4 @@
+package com.sweetcrust.team10_bakery.cart.domain.valueobjects;
+
+public class CartId {
+}
---
+++ src/test/java/com/sweetcrust/team10_bakery/cart/application/CartQueryServiceTest.java
@@ -0,0 +1,4 @@
+package com.sweetcrust.team10_bakery.cart.application;
+
+public class CartQueryServiceTest {
+}
